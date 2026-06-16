33dd0b389c1bf31deb7787419aa4eca215fd7c2b
Dev Dagar <ddagar321@gmail.com>
2026-06-16 15:05:03 +0530

    Update

---
+++ React-Day3/Notes.md
@@ -0,0 +1,4 @@
+Ques: Create 2 counter button. When You click on the fast counter button the pop will apear everytime and 
+when you cick on second counter button than popup should not come..
+
+#
---
+++ React-Day3/vite-project/src/App.jsx
@@ -0,0 +1,15 @@
+import React,{useState} from 'react'
+import UseEffectOne from './UseEffectOne'
+
+const App = () => {
+  
+  return (
+    <div>
+      <UseEffectOne />
+      <Timer />
+      
+    </div>
+  )
+}
+
+export default App
