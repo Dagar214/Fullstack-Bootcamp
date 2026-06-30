d316f6ab3174b2e1bec991ab67013d2f97582cd4
Dev Dagar <ddagar321@gmail.com>
2026-06-30 14:26:07 +0530

    updated MONGO day 2

---
+++ Mongo/Employee.mongodb.js
@@ -0,0 +1,54 @@
+use("employee")
+
+//////////////1.Create a collection named" employee and insert records in it
+//////////////key: name, empId, department, email, Salary, bonus
+
+
+//db.employee.insertMany([
+//  { name: "Amit", empId: 101, department: "IT", email: "amit@company.com", Salary: 50000, bonus: 5000 },
+//  { name: "Riya", empId: 102, department: "HR", email: "riya@company.com", Salary: 45000, bonus: 3000 },
+//  { name: "Karan", empId: 103, department: "Finance", email: "karan@company.com", Salary: 60000, bonus: 7000 }
+//])
+
+
+//////////////2.Read the records from the "employee" collection
+//db.employee.find()
+
+//////////////3.Update the salary of the employee with specific empId
+//db.employee.updateOne(
+//  { empId: 101 },
+//  { $set: { Salary: 55000 } }
+//)
+
+//////////////4.Delete the employee record with specific empId
+//db.employee.deleteOne({ empId: 103 })
+
+
+
+//////////aggregate
+db.employee.aggregate([
+  {
+    $sort: { Salary: -1 } // Sort by Salary in descending order
+    },
+    {
+        $project: {"name":1, "Salary":1, "department":1, "_id":
+            0, totalSalary: {$add: ["$Salary", "$bonus"]}} // Project only name, Salary, and department fields
+    }
+])
+
+//.employee.aggregate([
+//  {
+//    $group: {
+//      _id: "$department", // Group by department
+//      "totalSalary":{$sum: {$add: ["$Salary", "$bonus"]}}, // Calculate total salary for each department
+//    }
+//}])
+
+db.employee.aggregate([
+  {
+    $group: {
+      _id: "null", // Group all documents together
+      totalSalary: { $sum: { $add: ["$Salary", "$bonus"] } }
+    }
+  }
+])
--- Mongo/student.mongodb.js
+++ Mongo/student.mongodb.js
@@ -1,16 +1,16 @@
-use("user")
+//use("user")
 //create 
 // db.student.insertOne({
-// "name": "John Doe",
-//"rollNo":12344,
-// "section":"DS",
-// "email":"abc@abc.com"
+//    "name": "John Doe",
+//    "rollNo":12344,
+//    "section":"DS",
+//    "email":"abc@abc.com"
 // })
 
 //db.student.insertMany([
 //    {
 //        "name":"Danny",
-//        "rollNo":12345,
+//       "rollNo":12345,
 //        "section":"Aiml",
 //        "email":"def@def.com"
 //    },
@@ -26,5 +26,15 @@ use("user")
 //db.student.find()
 //db.student.find({"section"})
 //db.student.find({"rollNo":{$lte:12345 }})
-//db.student.find(find{}, {name:1, section:1,})
+//db.student.find({},{name:1, section:1, _id:1})
 //db.student.find().limit(2).skip(2)
+
+/////////////////Update
+//db.student.updateOne({"rollNo":12344},{$set:{"name":"John Doe","section":"DOS"}})
+//db.student.updateOne({"rollNo":12344},{$inc:{$inc:{"rollNo":1}}})
+
+////////////////Delete
+//db.student.deleteOne({"rollNo":12345})
+//db.student.deleteMany({"section":"Aiml"})
+
+
